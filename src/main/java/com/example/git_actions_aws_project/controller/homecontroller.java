924b0b363770ad41083f6c973c1513298385b2e8
muttanagouda <muthunsuman2025@gmail.com>
2026-08-15 21:32:41 +0530

    add file

---
+++ src/main/java/com/example/git_actions_aws_project/controller/homecontroller.java
@@ -0,0 +1,12 @@
+package com.example.git_actions_aws_project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class homecontroller {
+    @GetMapping("/")
+    public String home() {
+        return "App deployed successfully!";
+    }
+}
